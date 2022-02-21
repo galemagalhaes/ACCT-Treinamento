62a4ccb2027abd7d000dfeae1641ebdc242af91b
galemagalhaes <g.alemagalhaes@gmail.com>
2022-02-20 22:24:04 -0300

    atualiza react

--- estagioMod2/2.1-react/desafio-react/src/App.js
+++ estagioMod2/2.1-react/desafio-react/src/App.js
@@ -1,24 +1,40 @@
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  let rows = [];
-  var numero;
-  for (let i = 0; i < 10; i++) {
-    //rows.push(<h1 id={`${i + 1}`} className={`frase_${i}`}>Hello World!</h1>);    
-    rows.push(i);    
-  }
-  function clicar() {
-    console.log(rows)
-    numero = rows[4];
-    console.log(numero)
-  }
+
+  let verificacao = true;  
+  const [momentoDaConsulta, setMomentoDaConsulta] = useState(Date());
+  const [myVariable, setMyVariable] = useState("");
+  const [contNumero, setcontNumero] = useState([]); 
+
+  function clicou() {
+    setMyVariable("Hello World!")
+    setMomentoDaConsulta(Date())
+    do {
+       if (contNumero.length >= 10) 
+       {
+         setcontNumero(contNumero.splice((0), Number.MAX_VALUE));
+       }
+      
+      const rand = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+      if (!contNumero.includes(rand)) {
+        setcontNumero([...contNumero, rand]);
+         verificacao = false;
+      } else {
+        verificacao = true
+      }
+    } while (verificacao);
+    }
+
   return (
-    <div className="principal">
-      <span>
-        <h1 id={numero} className={`frase_${numero}`}>Hello World!</h1>
-      </span>
-      <button className="btn-troca-efeito" onClick={()=>{clicar()}}>Novo layout</button>
-    </div>   
+    <>
+      <div className='principal'>
+        <span className='imagem'>{ <h1 className={`frase_${contNumero[contNumero.length - 1]}`}>{myVariable}</h1>}</span>
+        <div className='div-relogio'><h2 className='txt-relogio'>{momentoDaConsulta}</h2></div>
+        <div className='div-botao'><button className='btn-troca-efeito' onClick={() => clicou()}>Novo layout</button></div>
+      </div>
+    </>
   );
 }
 
